d53e342e1446817e47441d53763aea307babe9b5
Tarekmohamed97 <tm7271420@gmail.com>
2020-11-02 23:34:58 +0200

    modefying some existing files to add another reducer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import CakeContainer from './components/cakeContainer'
 import {Provider} from 'react-redux';
 import store from './redux/store'
+import HooksCakeContainer from './components/hooksCakeContainer';
+import IcecreamContainer from './components/icecreamContainer';
 
 function App() {
   return (
     <Provider store = {store}>
       <div className="App">
+        <IcecreamContainer />
         <CakeContainer />
+        <HooksCakeContainer />
       </div>
     </Provider>
   );
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,1 +1,2 @@
 export {buyCake_action} from './cakes/cakeActions'
+export {buyIcecream_action} from './icecream/icecreamAction'
